bd5af079fd845287a72df271f0d85073ff4415e3
Qi <zhangqi444@gmail.com>
2021-09-05 22:57:01 -0700

    remove firebase deps

--- lib/service_locator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:toktik/screens/feed_viewmodel.dart';
-import 'package:get_it/get_it.dart';
-
-final locator = GetIt.instance;
-
-void setup() {
-  locator.registerSingleton<FeedViewModel>(FeedViewModel());
-}
