adba39fc27f5704e7424679c97a893620cd70e1d
Qi <zhangqi444@gmail.com>
2021-09-05 22:58:25 -0700

    remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_navigation/src/routes/transitions_type.dart';
-import 'package:toktik/service_locator.dart';
 
 import 'common/router_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  setup();
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     defaultTransition: Transition.rightToLeft,
