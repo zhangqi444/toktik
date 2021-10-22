13d631918e457afff278e5aca62083ad994f8505
Qi <zhangqi444@gmail.com>
2021-10-21 18:35:38 -0700

    update lambda layer to include shared library

---
+++ amplify/backend/function/toktikgraphqlLambdaLayer/opt/internal/index.js
@@ -0,0 +1,10 @@
+module.exports.constants = {
+    GRAPHQL_MODEL_NAME: {
+        LIKE: 'Like',
+        VIEW: 'View',
+        POST: 'Post',
+        SHARE: 'Share',
+        COMMENT: 'Comment',
+        USER: 'User',
+    }
+};
