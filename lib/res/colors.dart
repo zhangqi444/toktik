47f8b96b402b43fb4b7d23b6d1f5d0820f1325e0
zyc95 <zhaoyucheng@live.cn>
2021-11-29 22:46:01 +0800

    💄style: 新版个人信息样式

--- lib/res/colors.dart
+++ lib/res/colors.dart
@@ -6,4 +6,10 @@ class ColorRes{
   static const Color color_2 = Color.fromARGB(150, 255, 255, 255);
   static const Color color_3 = Color.fromARGB(255, 254,44,85);
   static const Color color_4 = Color.fromARGB(255, 250,206,21);
+  // 文字颜色
+  static const Color text_color = Color.fromRGBO(42, 42, 42, 1);
+  static const Color light_icon_color = Color.fromRGBO(42, 42, 42, 1);
+  static const Color light_background_color = Color.fromRGBO(255, 255, 255, 1);
+  static const Color tab_bottom_border_color = Color.fromRGBO(42, 42, 42, 1);
+  static const Color divider_color = Color.fromRGBO(238, 238, 238, 1);
 }
