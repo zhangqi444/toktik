b62300231ae9e2f10e5a55f6d01507908bf5a480
Qi <zhangqi444@gmail.com>
2022-02-12 15:18:05 -0800

    refactor the spinner widget.

---
+++ lib/page/widget/spinner_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class SpinnerWidget extends StatefulWidget {
+  bool show = false;
+  SpinnerWidget(bool show, {Key? key}) {
+    this.show = show;
+  }
+
+  @override
+  _SpinnerWidgetState createState() {
+    return _SpinnerWidgetState();
+  }
+
+}
+
+class _SpinnerWidgetState extends State<SpinnerWidget> {
+
+  @override
+  Widget build(BuildContext context) {
+    if(!widget.show) return Container();
+    print(MediaQuery.of(context).size);
+    return Align(
+        alignment: Alignment(0, -0.2),
+        child: Container(
+          width: 50,
+          height: 50,
+          child: SpinKitCircle(
+            //You can find other examples here.
+            //https://github.com/jogboms/flutter_spinkit/blob/master/example/lib/showcase.dart
+              color: Color(0xff00ffe0)
+          ),
+        )
+    );
+  }
+}
