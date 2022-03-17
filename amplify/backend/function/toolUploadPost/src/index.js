3384890cf8813159daf2bcfa00af98146248e9e2
Qi <zhangqi444@gmail.com>
2022-03-17 15:51:42 -0700

    update csv reading logic

--- amplify/backend/function/toolUploadPost/src/index.js
+++ amplify/backend/function/toolUploadPost/src/index.js
@@ -11,11 +11,29 @@
 	API_TOKTIK_USERTABLE_ARN
 Amplify Params - DO NOT EDIT */
 
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+const axios = require('axios');
+const gql = require('graphql-tag');
+const graphql = require('graphql');
+const { print } = graphql;
+const { query } = require('/opt/internal/utils/graphqlUtil');
+const { constants } = require('/opt/internal/index');
+const papa = require('papaparse'); 
+
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
+    var s3 = new AWS.S3();
+    var params = {
+        Bucket: STORAGE_S3TOKTIKSTORAGE55239E93_BUCKETNAME,
+        Key: "test.csv"
+    };
+    var csv = await s3.getObject(params);
+
+
+
     return {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
@@ -23,6 +41,6 @@ exports.handler = async (event) => {
     //      "Access-Control-Allow-Origin": "*",
     //      "Access-Control-Allow-Headers": "*"
     //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify(csv),
     };
 };
